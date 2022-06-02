d5ea20b46b92020db8de88de6e9e9fc2caaa26b0
kunxin-chor <chorkunxin@yahoo.com>
2022-06-02 08:09:02 +0000

    Added static files

--- 03-templates/index.js
+++ 03-templates/index.js
@@ -10,6 +10,10 @@ const app = express();
 // 2b. tell Express that we want to use hbs as the template engine
 app.set('view engine', 'hbs')
 
+// 2c. tell Express where to find static files
+// static files = browser javascript, images, css
+app.use(express.static('public')) // the 'public' refers to the 'public' folder
+
 // 3. put in the routes
 app.get('/', function(req,res){
     res.render("index.hbs")
